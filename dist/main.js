cc63ce3dbde940c1a9e8b324b4acfed970b58334
jimmyaffatigato <jimmyaffatigato@gmail.com>
2022-08-06 13:20:29 -0400

    Init

---
+++ dist/main.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const path = require("path");
+const distPath = process.argv[2];
+const PORT = 80;
+const app = express();
+app.use(express.static(distPath));
+app.get("/", (req, res) => {
+    const filePath = path.resolve(distPath, "index.html");
+    res.sendFile(filePath);
+});
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+});
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import express = require("express");
+import path = require("path");
+
+const distPath = process.argv[2];
+
+const PORT = 80;
+
+const app = express();
+app.use(express.static(distPath));
+
+app.get("/", (req, res) => {
+    const filePath = path.resolve(distPath, "index.html");
+    res.sendFile(filePath);
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+});
