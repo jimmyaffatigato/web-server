c0f6e0f5d78a02e4c4a9931abee43986aebfde1b
jimmyaffatigato <jimmyaffatigato@gmail.com>
2022-08-08 17:00:58 -0400

    Added Serve Script

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,10 @@
+#!/usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const path = require("path");
 const distPath = process.argv[2];
-const PORT = 80;
+const PORT = Number(process.argv[3]) || 80;
 const app = express();
 app.use(express.static(distPath));
 app.get("/", (req, res) => {
